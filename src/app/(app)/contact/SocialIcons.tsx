09e377a34ffa2df43f4000c746840de6736de753
tyler-Wiz <tylertooxclusive@gmail.com>
2024-07-17 13:19:30 +0100

    Update icon urls

--- src/app/(app)/contact/SocialIcons.tsx
+++ src/app/(app)/contact/SocialIcons.tsx
@@ -3,19 +3,28 @@ import Link from "next/link";
 
 const SocialIcons = () => (
   <div className="flex space-x-4 my-10 gap-3 text-primary">
-    <Link href="https://facebook.com" target="_blank" rel="noreferrer noopener">
+    <Link
+      href="https://www.facebook.com/WeplugMusicDistro"
+      target="_blank"
+      rel="noreferrer noopener">
       <FaFacebook className="text-4xl" />
     </Link>
-    <Link href="https://twitter.com" target="_blank" rel="noreferrer noopener">
+    <Link
+      href="https://x.com/weplugmusic"
+      target="_blank"
+      rel="noreferrer noopener">
       <FaTwitter className="text-4xl" />
     </Link>
     <Link
-      href="https://instagram.com"
+      href="https://www.instagram.com/weplugmusic_distro/"
       target="_blank"
       rel="noreferrer noopener">
       <FaInstagram className="text-4xl" />
     </Link>
-    <Link href="https://wa.me" target="_blank" rel="noreferrer noopener">
+    <Link
+      href="https://wa.link/qom9e7"
+      target="_blank"
+      rel="noreferrer noopener">
       <FaWhatsapp className="text-4xl" />
     </Link>
   </div>
